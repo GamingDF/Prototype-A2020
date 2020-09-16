ec5bc5e06638d7c7d135a75fa5f5a2329f95e5c1
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2020-09-16 18:18:13 -0300

    Add movement code

---
+++ PrototypeA/Assets/Scripts/Player_Input/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Movement : MonoBehaviour
+{
+    
+    float _input_x = 0f;
+    float _input_y = 0f;
+    public float speed = 5.5f;
+    bool _isWalking = false;
+    void Awake()
+    {
+        _isWalking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _input_x = Input.GetAxisRaw("Horizontal");
+        _input_y = Input.GetAxisRaw("Vertical");
+        _isWalking = (_input_x != 0 || _input_y != 0);
+
+        if(_isWalking){
+            var move = new Vector3(_input_x, _input_y, 0).normalized;
+            transform.position += move * speed * Time.deltaTime;
+        } 
+    }
+}
