200ea98d7ed1cb829df2c4f0e614612acda01fd0
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2020-10-28 14:36:18 -0300

    Destroy stone when vel ~= 0

--- PrototypeA/Assets/Scripts/Player_Input/Stone.cs
+++ PrototypeA/Assets/Scripts/Player_Input/Stone.cs
@@ -6,18 +6,25 @@ public class Stone : MonoBehaviour
 {
     public float speed = 2f;
     public float acceleration = -0.5f;
+
+    Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(speed < 0)
-            speed = 0;
-        else
-            speed += acceleration * Time.deltaTime;
+        rb.AddForce(new Vector2(-rb.velocity.x, -rb.velocity.y) * speed, ForceMode2D.Impulse);
+        Debug.Log("X: " + rb.velocity.x + " Y: " + rb.velocity.y);
+        if (rb.velocity.x < 0.05 && rb.velocity.x > -0.05)
+        {
+            if (rb.velocity.y < 0.05 && rb.velocity.y > -0.05)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
