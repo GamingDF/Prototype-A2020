44665d325accbb7ebe1c38b9bc4a3bf707b64655
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2020-10-28 14:35:48 -0300

    Create a base to rotete the ThrowPoint

---
+++ PrototypeA/Assets/Scripts/Player_Input/ThrowDirection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowDirection : MonoBehaviour
+{
+    public Camera cam;
+    public Rigidbody2D Player_rb;
+
+    Vector2 __mousePos;
+
+    void Update()
+    {
+        __mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 lookDir = __mousePos - Player_rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+    }
+}
